b2ac9b271f2e5e8f2ca97611d9f76931f6dd0588
ssocolow <ssocolow@github.com>
2020-10-25 20:47:56 -0400

    getting rid of wrong preprocessed data

--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
 #setup the repository so the htp model can be trained or run
 
 #first need to put together the preprocessed data for training (they are in pieces because of github's single file size limit)
-cat ./shtp_data_preprocessed/shtp_data.tar.gz.parta* > shtp_data.tar.gz
+cat ./shtp_data_processed/shtp_data_preprocessedparta* > shtp_data.tar.gz
 
 #then extract the preprocessed data
 tar -xvf shtp_data.tar.gz
 
+#move directories inside data dir
+mkdir data
+mv ETHhotel/ ETHuniv/ UCYuniv/ UCYzara01 UCYzara02 data/
+ 
 #tell the user they can run the model file now
 echo "Now you can run the command 'python3 ADI_model.py' to train the model"
